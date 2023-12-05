26fea2521f9312ccce0461a20f3cc2fcdc9186ba
Humingbird <sonugaahmed@gmail.com>
2023-12-05 15:57:26 +0100

    es6 promise

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,5 @@
+// Return a Promise rejecting with an Error and the string
+export default function uploadPhoto(filename) {
+    return Promise.reject(new Error(`${filename} cannot be processed`));
+  }
+
